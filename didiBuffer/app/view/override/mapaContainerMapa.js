77e84ce0002f73ba9263dfbd4984e1798e66462b
Gustavo Ramirez <gramirez@didicr.com>
2013-03-08 13:01:44 -0600

    primera modifc

--- didiBuffer/app/view/override/mapaContainerMapa.js
+++
@@ -1,7 +0,0 @@
-Ext.define('MyApp.view.override.mapaContainerMapa', {
-    override: 'MyApp.view.mapaContainerMapa',
-    getMapOptions: function(){
-    return Ext.merge({},this.options || this.getInitialConfig('mapOptions'));
-    }
-    
-});
--- didiBuffer/app/view/override/mapaSolicitudTaxi.js
+++
@@ -1,7 +0,0 @@
-Ext.define('MyApp.view.override.mapaSolicitudTaxi', {
-    override: 'MyApp.view.mapaSolicitudTaxi',
-    getMapOptions: function(){
-    return Ext.merge({},this.options || this.getInitialConfig('mapOptions'));
-    }
-    
-});
