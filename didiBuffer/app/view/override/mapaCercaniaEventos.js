14553ef52cfef917f8d7bb502d1d4571badabab7
Gustavo Ramirez <gramirez@didicr.com>
2013-03-14 08:08:56 -0600

    fqwreg

--- didiBuffer/app/view/override/mapaCercaniaEventos.js
+++
@@ -1,6 +0,0 @@
-Ext.define('MyApp.view.override.mapaCercaniaEventos', {
-    override: 'MyApp.view.mapaCercaniaEventos',
-    getMapOptions: function(){
-    return Ext.merge({},this.options || this.getInitialConfig('mapOptions'));
-    }
-});
